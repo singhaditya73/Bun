1fa16a7f2ffc79622623145dcd0bcda2ffbae4ef
aditya singh <adityasinghrajput755@gmail.com>
2024-11-01 18:26:14 +0530

    adding routes

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
-const read  = (text: string) =>
-{
-    return text
-}
-const msg = read("hello world")
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === '/') {
+            const body = "Hello, World!";
+            return new Response(body, { status: 200 });
+        } else {
+            return new Response("Not Found", { status: 404 });
+        }
+    }
+});
